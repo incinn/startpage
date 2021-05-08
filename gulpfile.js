3cca280310e4aee0d9d3ec5993b41dfeb47ab7f9
Barnz <hi@barnz.dev>
2021-05-09 00:39:24 +0100

    conditional sourcemaps

--- gulpfile.js
+++ gulpfile.js
@@ -1,15 +1,20 @@
 'use strict';
 require('dotenv').config();
+
 const { src, dest, watch } = require('gulp');
+const gulpif = require('gulp-if');
+const sourcemaps = require('gulp-sourcemaps');
 const sass = require('gulp-sass');
 const autoprefixer = require('gulp-autoprefixer');
 const cleanCSS = require('gulp-clean-css');
 
+const _PROD = process.env.MODE === 'production' ? true : false;
 const outputLocation = './dist';
 const sassLocation = './src/css/main.scss';
 
 function compileSass(done) {
     src(sassLocation)
+        .pipe(gulpif(!_PROD, sourcemaps.init()))
         .pipe(sass().on('error', sass.logError))
         .pipe(
             autoprefixer({
@@ -17,6 +22,7 @@ function compileSass(done) {
             })
         )
         .pipe(cleanCSS())
+        .pipe(gulpif(!_PROD, sourcemaps.write('.')))
         .pipe(dest(outputLocation));
     done();
 }
