cdc2a3063e5da51b68e868c5c05f125205fec3c5
Barnz <hi@barnz.dev>
2021-05-09 00:49:32 +0100

    compile pug with linter

--- gulpfile.js
+++ gulpfile.js
@@ -4,11 +4,15 @@ require('dotenv').config();
 const { src, dest, watch } = require('gulp');
 const gulpif = require('gulp-if');
 const sourcemaps = require('gulp-sourcemaps');
+const _PROD = process.env.MODE === 'production' ? true : false;
+
 const sass = require('gulp-sass');
 const autoprefixer = require('gulp-autoprefixer');
 const cleanCSS = require('gulp-clean-css');
 
-const _PROD = process.env.MODE === 'production' ? true : false;
+const pugLinter = require('gulp-pug-linter');
+const pug = require('gulp-pug-3');
+
 const outputLocation = './dist';
 const sassLocation = './src/css/main.scss';
 
@@ -31,5 +35,18 @@ function watchSass() {
     watch(sassLocation, compileSass);
 }
 
+function compilePug(done) {
+    src('./src/index.pug')
+        .pipe(pugLinter({ failAfterError: true, reporter: 'default' }))
+        .pipe(
+            pug({
+                extension: 'html',
+            })
+        )
+        .pipe(dest(outputLocation));
+    done();
+}
+
 exports.compileSass = compileSass;
 exports.watchSass = watchSass;
+exports.compilePug = compilePug;
