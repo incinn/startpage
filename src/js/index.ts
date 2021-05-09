dd355600ef81366941de3a11aa02ace1fd0f9983
Barnz <hi@barnz.dev>
2021-05-09 23:39:34 +0100

    welcome and time ts

--- src/js/index.ts
+++ src/js/index.ts
@@ -1,11 +1,31 @@
-interface Test {
-    id: number;
-    name: string;
+function setTime(): void {
+    const container = document.getElementById('timeContainer');
+
+    function pad(n: number): string | number {
+        return n < 10 ? '0' + n : n;
+    }
+
+    setInterval(() => {
+        const now = new Date();
+        container.innerHTML =
+            pad(now.getHours()) +
+            ':' +
+            pad(now.getMinutes()) +
+            ':' +
+            pad(now.getSeconds());
+    }, 1000);
 }
 
-function foo(): void {
-    let item: Test = { id: 1, name: 'testing' };
-    console.log(item);
+function welcomeMessage(): void {
+    const container = document.getElementById('welcomeContainer');
+    const now = new Date();
+
+    if (now.getHours() >= 0 && now.getHours() <= 11)
+        container.innerHTML = 'Morning';
+    else if (now.getHours() >= 12 && now.getHours() <= 17)
+        container.innerHTML = 'Afternoon';
+    else if (now.getHours() >= 18) container.innerHTML = 'Evening';
 }
 
-foo();
+welcomeMessage();
+setTime();
