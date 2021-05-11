c42afd23e6bdbb0f5f0fbf824b46e2596d5ecd07
Barnz <hi@barnz.dev>
2021-05-12 00:23:18 +0100

    refactor search plugin

--- src/js/components/search/search.ts
+++ src/js/components/search/search.ts
@@ -1,27 +1,43 @@
 import { SitePlugin } from '../site/plugin';
 
+export enum SearchMode {
+    assign, // keeps history
+    replace, // removes history
+}
+
 export class Search extends SitePlugin {
     public _name = 'Search';
     private searchInput: any;
+    private mode: SearchMode;
 
     constructor() {
         super();
+        this.mode = SearchMode.assign;
         this.searchInput = document.getElementById('searchInput');
     }
 
     public init(): void {
         this.searchInput.value = '';
 
-        this.searchInput.addEventListener('keyup', (event) => {
-            if (event.key === 'Enter') {
-                this.submitSearch();
-            }
+        this.searchInput.addEventListener('keyup', (event: KeyboardEvent) => {
+            this.handleKeyPress(event);
         });
+    }
+
+    private handleKeyPress(event: KeyboardEvent): void {
+        if (event.key === 'Enter') {
+            this.submitSearch();
+        }
     }
 
     private submitSearch(): void {
         const query = encodeURI(this.searchInput.value);
-        window.location.assign('https://duckduckgo.com/?q=' + query); // keeps history
-        // window.location.replace('https://duckduckgo.com/?q=' + query); // removes self from history
+        if (this.mode === SearchMode.assign) {
+            window.location.assign('https://duckduckgo.com/?q=' + query);
+        } else if (this.mode === SearchMode.replace) {
+            window.location.replace('https://duckduckgo.com/?q=' + query);
+        } else {
+            console.error(`Search mode ${this.mode} invalid`);
+        }
     }
 }
