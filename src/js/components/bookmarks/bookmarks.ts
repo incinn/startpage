4b24e7b7e233e67c27a57eacad6920e1bd912680
Barnz <hi@barnz.dev>
2021-07-03 01:49:04 +0100

    render bookmarks programmatically

---
+++ src/js/components/bookmarks/bookmarks.ts
@@ -0,0 +1,91 @@
+import { SitePlugin } from '../site/plugin';
+
+interface Bookmark {
+    favicon: string;
+    url: string;
+    text: string;
+}
+
+export class Bookmarks extends SitePlugin {
+    public _name = 'Bookmarks';
+    public _refresh = false;
+
+    private bookmarksContainerEl: HTMLElement;
+
+    private bookmarks: Bookmark[] = [
+        {
+            favicon: 'https://www.startpage.com/favicon.ico',
+            url: 'https://startpage.com',
+            text: 'startpage',
+        },
+        {
+            favicon: 'https://www.youtube.com/favicon.ico',
+            url: 'https://youtube.com',
+            text: 'youtube',
+        },
+        {
+            favicon: 'https://old.reddit.com/favicon.ico',
+            url: 'https://old.reddit.com',
+            text: 'old reddit',
+        },
+        {
+            favicon: 'https://www.duckduckgo.com/favicon.ico',
+            url: 'https://duckduckgo.com',
+            text: 'duckduckgo',
+        },
+        {
+            favicon: 'https://bbc.co.uk/favicon.ico',
+            url: 'https://bbc.co.uk',
+            text: 'bbc.co.uk',
+        },
+        {
+            favicon: 'https://www.startpage.com/favicon.ico',
+            url: 'https://startpage.com',
+            text: 'startpage',
+        },
+        {
+            favicon: 'https://news.ycombinator.com/favicon.ico',
+            url: 'https://news.ycombinator.com',
+            text: 'hacker news',
+        },
+    ];
+
+    constructor() {
+        super();
+
+        this.bookmarksContainerEl = document.getElementById(
+            'bookmarksContainer'
+        ) as HTMLElement;
+    }
+
+    public init(): void {
+        this.render();
+    }
+
+    private render(): void {
+        this.bookmarks.forEach((bookmark) => {
+            this.bookmarksContainerEl.appendChild(this.buildBookmark(bookmark));
+        });
+    }
+
+    private buildBookmark(b: Bookmark): HTMLElement {
+        const li = document.createElement('li');
+        const a = document.createElement('a');
+        const img = document.createElement('img');
+        const span = document.createElement('span');
+
+        a.appendChild(img);
+        a.appendChild(span);
+        li.appendChild(a);
+
+        a.href = b.url;
+        a.target = '_blank';
+        a.title = b.text;
+
+        img.src = b.favicon;
+
+        span.innerHTML = b.text;
+
+        return li;
+    }
+}
