bd716ae77b53d490bacfe248ea12f1b211babb00
Barnz <hi@barnz.dev>
2021-07-12 13:14:57 +0100

    refactor theme plugin

--- src/js/components/theme/theme.ts
+++ src/js/components/theme/theme.ts
@@ -1,16 +1,21 @@
-import { SitePlugin } from '../site/plugin';
+import { PluginStorage, SitePlugin } from '../site/plugin';
+
+export interface ThemeSettings {
+    name: string;
+    random: boolean;
+}
 
 export class DisplayTheme extends SitePlugin {
     public _name = 'Theme';
     private container: HTMLElement;
     private randomBtn: HTMLElement;
     private themeToggles: NodeListOf<HTMLElement>;
-    private theme: string = 'mountain';
     private themes = ['moon', 'mountain', 'cat', 'nlights'];
-    private data: any;
+    private settings: ThemeSettings;
 
     constructor() {
         super();
+
         this.container = document.body;
         this.themeToggles = document.querySelectorAll(
             '.themeSettings__options__toggle:not(#randomTheme)'
@@ -32,65 +37,73 @@ export class DisplayTheme extends SitePlugin {
 
         this.randomBtn.addEventListener('click', () => this.randomTheme());
 
-        this.getData();
-        this.data ? this.setTheme(this.data.data) : this.setRandomTheme();
+        const storage: PluginStorage = this.getStorage();
+        if (storage.data) {
+            this.settings = storage.data;
+        } else {
+            this.settings = {
+                name: 'mountain',
+                random: true,
+            };
+        }
 
-        this.showActiveTheme();
+        this.renderTheme();
     }
 
-    private getData(): void {
-        this.data = this.getStorage();
+    private updateStorage(settings): void {
+        const s: ThemeSettings = { ...this.settings, ...settings };
+
+        if (s.name !== this.settings.name) {
+            s.random = false;
+        }
+
+        this.setStorage({ lastChange: 0, data: s });
+        this.settings = s;
     }
 
-    private setTheme(theme: string): void {
+    private renderTheme(): void {
+        const theme: string = this.settings.random
+            ? this.themes[Math.floor(Math.random() * this.themes.length)]
+            : this.settings.name;
+
         this.themes.forEach((t) => {
-            this.container.classList.remove(t);
+            t === theme
+                ? this.container.classList.add(t)
+                : this.container.classList.remove(t);
         });
-        this.container.classList.add(theme);
-        this.theme = theme;
-        this.showActiveTheme();
+
+        this.updateSettingsButtonState();
     }
 
     private handleToggle(theme: string): void {
         if (this.themes.indexOf(theme) > -1) {
-            this.setStorage({ lastChange: 0, data: theme });
-            this.getData();
-            this.setTheme(theme);
+            this.updateStorage({ name: theme });
+            this.renderTheme();
         } else {
-            console.error('invalid theme');
+            console.error('invalid theme selected');
         }
     }
 
-    private showActiveTheme(): void {
+    private updateSettingsButtonState(): void {
         this.randomBtn.classList.remove('selected');
 
         this.themeToggles.forEach((toggle) => {
             toggle.classList.remove('selected');
-            toggle.classList.remove('active');
 
-            if (toggle.dataset.id === this.theme) {
-                toggle.classList.add('active');
-            }
-
-            if (this.data && this.data.data == toggle.dataset.id) {
-                toggle.classList.add('selected');
+            if (!this.settings.random) {
+                if (toggle.dataset.id === this.settings.name) {
+                    toggle.classList.add('selected');
+                }
             }
         });
 
-        if (!this.data) {
+        if (!this.settings || !this.settings.name || this.settings.random) {
             this.randomBtn.classList.add('selected');
         }
     }
 
-    private setRandomTheme(): void {
-        this.setTheme(
-            this.themes[Math.floor(Math.random() * this.themes.length)]
-        );
-    }
-
     private randomTheme(): void {
-        this.destroyStorage();
-        this.getData();
-        this.setRandomTheme();
+        this.updateStorage({ random: true });
+        this.renderTheme();
     }
 }
