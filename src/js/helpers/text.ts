388510c689d7765a96511f7b4d95087d90e9e711
Barnz <hi@barnz.dev>
2021-07-12 12:13:47 +0100

    refactor and cleanup weather plugin

--- src/js/helpers/text.ts
+++ src/js/helpers/text.ts
@@ -1,3 +1,7 @@
 export function capitalizeFirstLetter(text: string): string {
     return text.charAt(0).toUpperCase() + text.slice(1);
 }
+
+export function azStringClean(text: string): string {
+    return text.replace(/[^a-zA-Z ]/gi, '');
+}
